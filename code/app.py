e500295caf80ee663d765d9ae47fd2044dc444f8
hjcho <hjcho9730@gmail.com>
2025-11-11 22:04:29 +0900

    서버 개발 중

---
+++ code/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+from modelLoad import modelLoad, preparePre, file_name
+from func.utils import *
+from func.main_func import process_data, res
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/api/getResult", methods=["POST"])
+def process():
+    data = request.get_json()
+    user_input = data.get("input", "")
+    
+    # 여기에 원하는 처리 로직 넣기 (AI, 수식, 텍스트 처리 등)
+    s= user_input
+
+    new_vec= process_data([s], getVec_steps)
+    new_vec = toArray(new_vec)
+
+    predictions = loadedModel.predict(new_vec)
+    label =  res[int(predictions[0])]
+    print(f'"{s}" → {label}')
+
+    return jsonify({"result": label})
+
+if __name__ == "__main__":
+    fileName= file_name()
+    path = os.path.join(real_path, "models", fileName)
+    loadedModel= modelLoad(path=path)
+    getVec_steps= preparePre()
+
+    app.run(debug=True)
